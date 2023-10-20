471caf030f59cfba6254cbc9538ffa911c537360
Humberto Pereira <h.pereira@Humbertos-MacBook-Pro.fritz.box>
2023-10-20 18:46:00 +0200

    Add admin super-user and SummerNote

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Recipe, Rating
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
+
+
+admin.site.register(Rating)
