7e5c367b8e03f36b10832f0af15d70854d164068
Humberto Pereira <h.pereira@Humbertos-MacBook-Pro.fritz.box>
2023-10-20 17:22:06 +0200

    Re-installing packages 1 of 2

--- venv/bin/django-admin.py
+++ venv/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/h.pereira/Projects/lekker-cook/lekker-cook/venv/bin/python3.12
+#!/Users/h.pereira/Projects/lekker-cook/lekker-cook/venv/bin/python3.9
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
