8f7a26643ec84fd18b618a91f93a0d7af6b4d383
Humberto Pereira <humberto_pereira@live.com>
2023-10-24 11:59:15 +0200

    Add RecipeView to view.py and HTML templates

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Recipe, Rating, Comment, Favorite, Category
+from .models import Recipe, Rating, Comment, Favorite, Category, CommentLike
 
+
+# Recipe View: Handles the recipe posts
+class RecipeView(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status='published').order_by('-all_ratings')
+    template_name = 'index.html'
 
