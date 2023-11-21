9c93a7adc7d6fc5838dc311bc40cb3edbc2acbf0
Humberto Pereira <humberto_pereira@live.com>
2023-11-21 13:26:00 +0100

    Deployment commit

--- lekkercook/settings.py
+++ lekkercook/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['lekker-cook-00ac4ef685fa.herokuapp.com', '127.0.0.1']
 
